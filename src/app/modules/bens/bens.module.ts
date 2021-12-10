b306f069bc0bfe694853a9d49057d1189fa7be30
GiulianaCDA <giulianaarecippo@hotmail.com>
2021-12-10 03:03:51 -0300

    update forms

--- src/app/modules/bens/bens.module.ts
+++ src/app/modules/bens/bens.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { BensRoutingModule } from './bens-routing.module';
 import { IndexComponent } from './components/index/index.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { CreateComponent } from './components/create/create.component';
 
 
 
 @NgModule({
   declarations: [
     IndexComponent,
-    CreateComponent
   ],
   imports: [
     CommonModule,
--- src/app/modules/bens/components/index/index.component.ts
+++ src/app/modules/bens/components/index/index.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import {Bem} from '../../shared/bem.model'
 import { BensService } from '../../shared/services/bens.service';
 import { Location } from '@angular/common';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-index',
@@ -11,18 +13,49 @@ import { Location } from '@angular/common';
 export class IndexComponent implements OnInit {
 
   bens: Bem[];
+  bensForm: FormGroup;
 
-  constructor(private bensService: BensService ) { }
+  constructor(private bensService: BensService, private location: Location ) { }
 
+  sucess:boolean = false
+  
   ngOnInit(): void {
     this.bensService.getAll()
     .subscribe(f => this.bens = f)
+
+    const date = new Date
+    const today = String(date.getFullYear()) + '-' + String(date.getMonth()+1) + '-' + String(date.getDate())
+    this.bensForm = new FormGroup({
+
+      'marca': new FormControl('', [Validators.required]), 
+      'quantidade': new FormControl('', [Validators.required]),
+      'valor_unitario': new FormControl('', [Validators.required]), 
+      'inicio_garantia': new FormControl('', [Validators.required]),
+      'termino_garantia': new FormControl('', [Validators.required]),
+      'observacoes': new FormControl(''),
+      'estado': new FormControl('Bom'), 
+      'situacao': new FormControl('Em uso'),
+      'data_cadastro': new FormControl(today),
+      'tombamento': new FormControl('')
+      
+    })
   }
 
-  del(bem: Bem){
+  del(bem: Bem, i){
+    if (confirm("Tem certeza que deseja remover este bem?"))
     this.bensService.delete(bem) 
-    .subscribe( _ => location.reload())
+    .subscribe( _ => {
+      this.bens.splice(i, 1)
+    })
   }
 
+  onSubmit(){
+    const newBem = this.bensForm.value
+    newBem.tombamento = newBem.termino_garantia.split('-')[0] + '123456'
+
+    this.bensService.create(newBem).subscribe( _ => {
+      this.sucess = true
+    })
+  }
 
 }
